0d9d8871749de0f4180b0d83cd47f68cdf953f7d
mylee1124 <mylee1124@gmail.com>
2015-09-07 16:12:05 +0900

    ArrayList

---
+++ ArrayList/ArrayList.h
@@ -0,0 +1,34 @@
+/*
+ * ArrayList.h
+ *
+ *  Created on: 2015. 8. 27.
+ *      Author: User
+ */
+
+#ifndef ARRAYLIST_H_
+#define ARRAYLIST_H_
+
+#define TRUE 1
+#define FALSE 0
+
+#define LIST_LEN 100
+typedef int LData;
+
+typedef struct __ArrayList
+{
+	LData arr[LIST_LEN];
+	int numOfData;
+	int curPosition;
+}ArrayList;
+
+typedef ArrayList List;
+
+void ListInit(List * plist);
+void LInsert(List * plist, LData data);
+
+int LFirst(List * plist, LData * pdata);
+int LNext(List * plist, LData * pdata);
+
+LData LRemove(List * plist);
+int LCount(List * plist);
+#endif /* ARRAYLIST_H_ */
